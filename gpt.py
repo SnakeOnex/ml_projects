e2f28c72996150731d60e149f896dc5e519a582e
SnakeOneX <romakcz@gmail.com>
2024-07-19 15:45:05 +0200

    training loop + params rewrite

--- gpt.py
+++ gpt.py
@@ -3,61 +3,15 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-n_embd = 384
-n_head = 6
-n_layer = 4
 dropout = 0.2
 # ------------
 
 torch.manual_seed(1337)
 
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-# with open('input.txt', 'r', encoding='utf-8') as f:
-    # text = f.read()
-
-# # here are all the unique characters that occur in this text
-# chars = sorted(list(set(text)))
-# vocab_size = 512
-# # create a mapping from characters to integers
-# stoi = { ch:i for i,ch in enumerate(chars) }
-# itos = { i:ch for i,ch in enumerate(chars) }
-# encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-# decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
-
-# Train and test splits
-# data = torch.tensor(encode(text), dtype=torch.long)
-# n = int(0.9*len(data)) # first 90% will be train, rest val
-# train_data = data[:n]
-# val_data = data[n:]
-
-# data loading
-def get_batch(split):
-    # generate a small batch of data of inputs x and targets y
-    data = train_data if split == 'train' else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    x, y = x.to(device), y.to(device)
-    return x, y
-
-@torch.no_grad()
-def estimate_loss():
-    out = {}
-    model.eval()
-    for split in ['train', 'val']:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            X, Y = get_batch(split)
-            logits, loss = model(X, Y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    model.train()
-    return out
-
 class Head(nn.Module):
     """ one head of self-attention """
 
-    def __init__(self, head_size, block_size):
+    def __init__(self, head_size, block_size, n_embd):
         super().__init__()
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
@@ -85,9 +39,9 @@ class Head(nn.Module):
 class MultiHeadAttention(nn.Module):
     """ multiple heads of self-attention in parallel """
 
-    def __init__(self, num_heads, head_size, block_size):
+    def __init__(self, num_heads, head_size, block_size, n_embd):
         super().__init__()
-        self.heads = nn.ModuleList([Head(head_size, block_size) for _ in range(num_heads)])
+        self.heads = nn.ModuleList([Head(head_size, block_size, n_embd) for _ in range(num_heads)])
         self.proj = nn.Linear(head_size * num_heads, n_embd)
         self.dropout = nn.Dropout(dropout)
 
@@ -118,7 +72,7 @@ class Block(nn.Module):
         # n_embd: embedding dimension, n_head: the number of heads we'd like
         super().__init__()
         head_size = n_embd // n_head
-        self.sa = MultiHeadAttention(n_head, head_size, block_size)
+        self.sa = MultiHeadAttention(n_head, head_size, block_size, n_embd)
         self.ffwd = FeedFoward(n_embd)
         self.ln1 = nn.LayerNorm(n_embd)
         self.ln2 = nn.LayerNorm(n_embd)
@@ -130,17 +84,21 @@ class Block(nn.Module):
 
 class GPTLanguageModel(nn.Module):
 
-    def __init__(self, block_size, vocab_size):
+    def __init__(self, block_size, vocab_size, n_embd, n_head, n_layer):
         super().__init__()
         self.block_size = block_size
         self.vocab_size = vocab_size
+        self.n_embd = n_embd
+        self.n_head = n_head
+        self.n_layer = n_layer
 
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(self.vocab_size, n_embd)
-        self.position_embedding_table = nn.Embedding(self.block_size, n_embd)
-        self.blocks = nn.Sequential(*[Block(n_embd, n_head, self.block_size) for _ in range(n_layer)])
-        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
-        self.lm_head = nn.Linear(n_embd, self.vocab_size)
+        print(self.vocab_size, self.n_embd)
+        self.token_embedding_table = nn.Embedding(self.vocab_size, self.n_embd)
+        self.position_embedding_table = nn.Embedding(self.block_size, self.n_embd)
+        self.blocks = nn.Sequential(*[Block(self.n_embd, self.n_head, self.block_size) for _ in range(self.n_layer)])
+        self.ln_f = nn.LayerNorm(self.n_embd) # final layer norm
+        self.lm_head = nn.Linear(self.n_embd, self.vocab_size)
 
         # better init, not covered in the original GPT video, but important, will cover in followup video
         self.apply(self._init_weights)
---
+++ train_gpt.py
@@ -0,0 +1,180 @@
+import argparse, time, time, tqdm, PIL, wandb
+import torch, torch.nn as nn, torchvision
+from pathlib import Path
+from torch.utils.data import DataLoader
+from vqvae import VQVAE
+from model_configs import model_configs
+from gpt import GPTLanguageModel
+from utils import get_free_gpu
+
+device = torch.device(get_free_gpu())
+print("selected device: ", device)
+
+def generate_sample(path):
+    images = torch.zeros((0,C,SZ,SZ)).to(device)
+    for _ in range(16):
+        context = torch.zeros((1, 1), dtype=torch.long, device=device)
+        idx = torch.randint(0, tokens.shape[0], (1,))
+        context[0, 0] = tokens[idx,0]
+
+        res = gpt.generate(context, IMAGE_TOKENS-1)
+        img = model.decode(res)
+
+        if args.dataset == "imagenet":
+            mean = torch.tensor([0.485, 0.456, 0.406]).reshape(1, C, 1, 1).to(device)
+            std = torch.tensor([0.229, 0.224, 0.225]).reshape(1, 3, 1, 1).to(device)
+            img = img * std + mean
+            img = torch.clamp(img, 0, 1)
+        images = torch.cat([images, img], dim=0)
+
+    grid_pred = torchvision.utils.make_grid(images, nrow=4)
+    grid_final = grid_pred.permute(1, 2, 0)
+
+    grid_final = grid_final.cpu().detach().numpy()
+    grid_final = (grid_final * 255).astype("uint8")
+    grid_final = PIL.Image.fromarray(grid_final)
+    grid_final.save(path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, default="mnist")
+    args = parser.parse_args()
+
+    run_name = f"gpt-{args.dataset}-{time.time():.0f}"
+    run_folder = Path("runs") / run_name
+    run_folder.mkdir(exist_ok=True, parents=True)
+
+    config = model_configs[args.dataset]
+    C, SZ, K, D = config["channels"], config["image_sz"], config["K"], config["D"]
+    IMAGE_TOKENS = (SZ//4)**2
+    block_size = IMAGE_TOKENS-1
+    batch_size = 128
+    eval_iters = 50
+    eval_interval = 100
+    max_iters = 500000
+    print(f"dataset={args.dataset}, {C=}, {SZ=}, {IMAGE_TOKENS=}, {block_size=}, {batch_size=}\
+            {eval_iters=}, {max_iters=}, {K=} {D=}")
+
+    train_dataset, test_dataset = config["fetch_train"](), config["fetch_test"]()
+    print(f"train_sz={len(train_dataset)}, test_sz={len(test_dataset)}")
+
+    train_loader = DataLoader(
+            train_dataset, 
+            batch_size=256, 
+            shuffle=True, 
+            num_workers=16, 
+            prefetch_factor=4, 
+            pin_memory=True,
+            persistent_workers=False
+    )
+    test_loader = DataLoader(
+            test_dataset, 
+            batch_size=256, 
+            shuffle=False, 
+            num_workers=16, 
+            prefetch_factor=4, 
+            pin_memory=True,
+            persistent_workers=False
+    )
+
+    model = VQVAE(config).to(device)
+    model.load_state_dict(torch.load(f"checkpoints/{args.dataset}_best.pth"))
+
+    tokens = torch.zeros((0,IMAGE_TOKENS), dtype=torch.long, device=device)
+    for x, _ in tqdm.tqdm(test_loader):
+        x = x.to(device)
+        quantized = model(x)["closest"]
+        tokens = torch.cat([tokens, quantized], dim=0)
+
+    for x, _ in tqdm.tqdm(train_loader):
+        x = x.to(device)
+        quantized = model(x)["closest"]
+        tokens = torch.cat([tokens, quantized], dim=0)
+
+    tokens = tokens[:1000000]
+    tokens = tokens[torch.randperm(tokens.shape[0])]
+
+    print("tokens shape: ", tokens.shape)
+    print("tokens_count: ", tokens.view(-1).shape[0])
+
+    gpt_config = {
+        "block_size": block_size,
+        "vocab_size": K,
+        "n_embd": 768,
+        "n_head": 6,
+        "n_layer": 4,
+    }
+
+    gpt = GPTLanguageModel(**gpt_config).to(device)
+    gpt.train()
+
+    wandb.init(project="gpt-vqvae",
+               name=run_name,
+               config={"dataset": args.dataset, 
+                       "block_size": block_size, 
+                       "batch_size": batch_size, 
+                       "max_iters": max_iters,
+                       "K": K,
+                       "SZ": SZ,
+                       "C": C,
+                       "tokens_shape": tokens.shape,
+                       "total_token_count": tokens.view(-1).shape[0]})
+    wandb.watch(gpt)
+
+    optim = torch.optim.AdamW(gpt.parameters(), lr=1e-3)
+
+    split = int(0.8 * len(tokens))
+    train_tokens, val_tokens = tokens[:split], tokens[split:]
+
+    def get_batch(data):
+        # generate a small batch of data of inputs x and targets y
+        ix = torch.randint(data.shape[0], (batch_size,))
+        x = torch.stack([data[i,0:block_size] for i in ix])
+        y = torch.stack([data[i,1:block_size+1] for i in ix])
+        x, y = x.to(device), y.to(device)
+        return x, y
+
+    @torch.no_grad()
+    def estimate_loss():
+        out = {}
+        model.eval()
+        for split in ['train', 'val']:
+            losses = torch.zeros(eval_iters)
+            for k in range(eval_iters):
+                X, Y = get_batch(train_tokens if split == 'train' else val_tokens)
+                logits, loss = gpt(X, Y)
+                losses[k] = loss.item()
+            out[split] = losses.mean()
+        model.train()
+        return out
+
+    Path("gens").mkdir(exist_ok=True)
+
+    best_val_loss = float('inf')
+
+    for iter in range(max_iters):
+        # every once in a while evaluate the loss on train and val sets
+        if iter % eval_interval == 0 or iter == max_iters-1:
+            losses = estimate_loss()
+            print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+            generate_sample(run_folder / f"{args.dataset}_generated_{iter}.png")
+            wandb.log({"train_loss": losses["train"], "val_loss": losses["val"]})
+            torch.save({"model": model.state_dict(), "optim": optim.state_dict(), "step": iter},
+                       run_folder / f"checkpoint_{iter}.pth")
+
+            if iter % 500 == 0: wandb.save(run_folder / f"{args.dataset}_generated_{iter}.png")
+            if losses["val"] < best_val_loss:
+                best_val_loss = losses["val"]
+                torch.save({"model": model.state_dict(), "optim": optim.state_dict(), "step": iter},
+                       run_folder / f"best_model.pth")
+
+        # sample a batch of data
+        xb, yb = get_batch(train_tokens)
+
+        # evaluate the loss
+        logits, loss = gpt(xb, yb)
+        optim.zero_grad(set_to_none=True)
+        loss.backward()
+        optim.step()
+
+    torch.save(gpt.state_dict(), f"checkpoints/{args.dataset}_gpt.pth")
