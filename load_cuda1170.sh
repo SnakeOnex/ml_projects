83f2b04eb3ea8fa733159de2947e6eb63dcf909c
snakeonex <romakcz@gmail.com>
2024-07-24 12:15:21 +0200

    load triton script

---
+++ load_cuda1170.sh
@@ -0,0 +1,5 @@
+ml load Z3/4.10.2-GCCcore-11.3.0-Python-3.10.4
+ml load Triton/2.1.0-foss-2022a-CUDA-11.7.0
+ml load PyTorch/2.1.0-foss-2022a-CUDA-11.7.0
+ml load Python/3.10.4-GCCcore-11.3.0
+ml load torchvision/0.16.0-foss-2022a-CUDA-11.7.0
