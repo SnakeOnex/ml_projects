eae4035b6af074d2358847763c3fb899ee2721ab
SnakeOnex <romakcz@gmail.com>
2024-09-07 20:23:24 +0200

    idk

--- load_cuda1170.sh
+++ load_cuda1170.sh
@@ -4,3 +4,4 @@ ml load PyTorch/2.1.0-foss-2022a-CUDA-11.7.0
 ml load Python/3.10.4-GCCcore-11.3.0
 ml load torchvision/0.16.0-foss-2022a-CUDA-11.7.0
 ml load matplotlib/3.5.2-foss-2022a
+ml load PyTorch-Lightning/2.1.0-foss-2022a-CUDA-11.7.0
