e224f444c011131607afe015c5417840021ddb76
SnakeOnex <romakcz@gmail.com>
2024-10-02 12:34:50 +0200

    classifier free guidance

--- decode_gpt.py
+++ decode_gpt.py
@@ -7,51 +7,51 @@ from tqdm import trange
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-def sample(logits, temperature: float=1.0, top_k: int=0, top_p: float=1.0, sample_logits=True):        
-    logits = logits[:, -1, :] / max(temperature, 1e-5)
-    if top_k > 0 or top_p < 1.0:
-        logits = top_k_top_p_filtering(logits, top_k=top_k, top_p=top_p)
-    probs = F.softmax(logits, dim=-1)
-    if sample_logits:
-        idx = torch.multinomial(probs, num_samples=1)
+def decode_one_token(model, x, input_pos, cfg_scale=1.0):
+    if cfg_scale == 1.0:
+        logits, _ = model(x, cond_idx=None, input_pos=input_pos)
     else:
-        _, idx = torch.topk(probs, k=1, dim=-1)
-    return idx, probs
-
-def decode_one_token(model, x, input_pos):
-    logits, _ = model(x, cond_idx=None, input_pos=input_pos)
+        x_comb = torch.cat([x,x])
+        logits, _ = model(x_comb, cond_idx=None, input_pos=input_pos)
+        cond_logits = logits[:x.shape[0], ...]
+        uncond_logits = logits[x.shape[0]:, ...]
+        logits = uncond_logits + (cond_logits - uncond_logits) * cfg_scale
     probs = F.softmax(logits[:, -1, :], dim=-1)
     return torch.multinomial(probs, num_samples=1)
 
-def prefill(model, cond, input_pos):
-    logits, _ = model(None, cond_idx=cond, input_pos=input_pos)
+def prefill(model, cond, input_pos, cfg_scale=1.0):
+    if cfg_scale == 1.0:
+        logits, _ = model(None, cond_idx=cond, input_pos=input_pos)
+    else:
+        cond_null = torch.ones_like(cond) * 1000
+        cond_comb = torch.cat([cond, cond_null])
+        logits, _ = model(None, cond_idx=cond_comb, input_pos=input_pos)
+        cond_logits = logits[:cond.shape[0], ...]
+        uncond_logits = logits[cond.shape[0]:, ...]
+        logits = uncond_logits + (cond_logits - uncond_logits) * cfg_scale
     probs = F.softmax(logits[:, -1, :], dim=-1)
     return torch.multinomial(probs, num_samples=1)
 
 @torch.no_grad()
-def generate(model, cond, max_new_tokens):
+def generate(model, cond, max_new_tokens, cfg_scale):
     T = 1 + max_new_tokens # cond + new tokens
-    max_batch_size = cond.shape[0]
+    max_batch_size = cond.shape[0] if cfg_scale == 1.0 else cond.shape[0] * 2
 
     device = cond.device
     with torch.device(device):
         model.setup_caches(max_batch_size=max_batch_size, max_seq_length=T, dtype=model.tok_embeddings.weight.dtype)
 
-
     # 1. prefill input_seq and K/V caches with the conditioning tokens
-    seq = torch.empty((max_batch_size, T-1), dtype=torch.int, device=device)
+    seq = torch.empty((cond.shape[0], T-1), dtype=torch.int, device=device)
     input_pos = torch.arange(0, 1, device=device)
-    next_token = prefill(model, cond, input_pos)
+    next_token = prefill(model, cond, input_pos, cfg_scale)
     seq[:, 0:1] = next_token
-    print("seq: ", seq.shape)
-    print(f"{seq[:, 0:1]=}")
 
     # 2. generate new tokens
     input_pos = torch.tensor([1], device=device, dtype=torch.int)
     new_tokens = []
     for t in range(max_new_tokens-1):
-        next_token = decode_one_token(model, next_token, input_pos)
+        next_token = decode_one_token(model, next_token, input_pos, cfg_scale)
         input_pos += 1
         new_tokens.append(next_token.clone())
         next_token = next_token.view(-1, 1)
@@ -71,17 +71,20 @@ if __name__ == "__main__":
     vqgan_path = "runs_vqgan/vqgan-imagenet-1726089582/best.pth"
     vqgan.load_state_dict(torch.load(vqgan_path))
     bs = 16
+    cfg_scale = 2.0
 
     st = time.time()
-    for i in trange(1000):
+    for i in trange(0,1000):
         cond = torch.zeros((bs,), dtype=torch.long, device=device)
         cond += i
-        output_tokens = generate(gpt, cond, 256)
+        output_tokens = generate(gpt, cond, 256, cfg_scale)
+        filename = f"samples/{i}{'_cfg' if cfg_scale>1.0 else ''}.png"
 
         images = (denormalize(vqgan.decode(output_tokens)) * 255).to(dtype=torch.uint8)
         grid = torchvision.utils.make_grid(images, nrow=4).permute(1, 2, 0).cpu().detach().numpy()
         grid_final = PIL.Image.fromarray(grid)
-        grid_final.save(f"samples/{i}.png")
+        grid_final.save(filename)
+        print(f"Saved {filename}")
 
     dur = time.time()-st
     print(f"Time taken: {dur:.2f} seconds")
