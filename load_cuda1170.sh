9df61386487c86b0273e18293b2d74ac316e9ad9
SnakeOnex <romakcz@gmail.com>
2024-10-09 09:47:18 +0200

    removed garbage

--- load_cuda1170.sh
+++
@@ -1,7 +0,0 @@
-ml load Z3/4.10.2-GCCcore-11.3.0-Python-3.10.4
-ml load Triton/2.1.0-foss-2022a-CUDA-11.7.0
-ml load PyTorch/2.1.0-foss-2022a-CUDA-11.7.0
-ml load Python/3.10.4-GCCcore-11.3.0
-ml load torchvision/0.16.0-foss-2022a-CUDA-11.7.0
-ml load matplotlib/3.5.2-foss-2022a
-ml load PyTorch-Lightning/2.1.0-foss-2022a-CUDA-11.7.0
--- load_modules.sh
+++ load_modules.sh
@@ -1,7 +1,9 @@
-ml load Z3/4.13.0-GCCcore-13.2.0
 # ml load Triton/2.1.0-foss-2022a-CUDA-11.7.0
-ml load PyTorch/2.4.0-foss-2023b-CUDA-12.4.0
 ml load Python/3.11.5-GCCcore-13.2.0
+ml load PyTorch/2.4.0-foss-2023b-CUDA-12.4.0
+ml load Z3/4.13.0-GCCcore-13.2.0
 ml load torchvision/0.19.0-foss-2023b-CUDA-12.4.0
 ml load matplotlib/3.8.2-gfbf-2023b
-ml load PyTorch-Lightning/2.4.0-foss-2023b-CUDA-12.4.0
+ml load OpenSSL/3
+
+#ml load PyTorch-Lightning/2.4.0-foss-2023b-CUDA-12.4.0
